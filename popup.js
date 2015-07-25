3524fb89e172d9635685dabace389858d5aa1344
Xelnect <dido.jordanov7@gmail.com>
2015-07-25 16:41:12 +0300

    fsdf

---
+++ popup.js
@@ -0,0 +1,30 @@
+/**
+ * Created by XelnectPC on 7/25/2015.
+ */
+document.addEventListener('DOMContentLoaded', function() {
+    var checkPageButton = document.getElementById('checkPage');
+    checkPageButton.addEventListener('click', function() {
+
+        chrome.tabs.getSelected(null, function(tab) {
+            d = document;
+
+            var f = d.createElement('form');
+            f.action = 'http://gtmetrix.com/analyze.html?bm';
+            f.method = 'post';
+            var i = d.createElement('input');
+            i.type = 'hidden';
+            i.name = 'url';
+            i.value = tab.url;
+            f.appendChild(i);
+            d.body.appendChild(f);
+            f.submit();
+        });
+    }, false);
+    var checkPageButton2 = document.getElementById('checkPage2');
+    checkPageButton2.addEventListener('click', function() {
+
+        var newURL = "http://www.google.nl";
+        chrome.tabs.create({ url: newURL });
+
+    }, false);
+}, false);
